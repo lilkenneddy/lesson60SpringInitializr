b79c586fb5d7cbb9529201e0c4d2b945497657e3
Rostyslav Skliaruk <sklyaruk.rostislav1903@gmail.com>
2025-05-28 15:38:56 +0200

    completed homework

---
+++ src/main/java/com/codillas/loyalty_card/LoyaltyCardApplication.java
@@ -0,0 +1,13 @@
+package com.codillas.loyalty_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codillas/loyalty_card/LoyaltyCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codillas.loyalty_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
